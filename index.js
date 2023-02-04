1848c45c9c8b381f5287752e713bae3975b7fa18
Have Samuel <samiehave9@gmail.com>
2023-02-04 04:38:13 +0300

    Returning Functions

--- index.js
+++ index.js
@@ -20,16 +20,66 @@
 // }
 
 // Storing functions in variables
-function greet() {
-  console.log('Hi!, i Love You!!')
+// function greet() {
+//   console.log('Hi!, i Love You!!')
+// }
+
+// function diss() {
+//   console.log('Your The waste!!');
+// }
+
+// const funcs = [greet, diss];
+
+// const myFunc = function add(x, y) {
+//   return x + y;
+// }
+
+// Return value of a function can be another function
+// function giveBirth() {
+//   return function cry() {
+//     return 'WAAAAAH!!'
+//   }
+// }
+// // In the console
+// giveBirth()
+// // OR
+// giveBirth()()
+// // OR
+// const func = giveBirth()
+// // func() => WAAAAAH!!
+
+function makeMultipleFunc(num) {
+  return function mul(x) {
+    return num * x;
+  }
 }
 
-function diss() {
-  console.log('Your The waste!!');
-}
-
-const funcs = [greet, diss];
-
-const myFunc = function add(x, y) {
-  return x + y;
-}
+// makeMultipleFunc()
+//  mul(x) {
+//     return num * x;
+//   }
+// const quad = makeMultipleFunc(4)
+// quad
+//  mul(x) {
+//     return num * x;
+//   }
+// const double = makeMultipleFunc(2)
+// undefined
+// double
+//  mul(x) {
+//     return num * x;
+//   }
+// double(4)
+// 8
+// double(419)
+// 838
+// double(41)
+// 82
+// quad(4)
+// 16
+// quad(2)
+// 8
+// quad(4)
+// 16
+// quad(3)
+// 12
