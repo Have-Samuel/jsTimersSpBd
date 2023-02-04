281b353d39b4bdd6c715da2b56f36171121ea329
Have Samuel <samiehave9@gmail.com>
2023-02-04 04:05:49 +0300

    Storing functions in arrays

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+// // Using functions as Arguements
+// function greet() {
+//   console.log('Hi!, i Love You!!')
+// }
+
+// function diss() {
+//   console.log('Your The waste!!');
+// }
+
+// function repeatThreeTimes(func) {
+//   func();
+//   func();
+//   func();
+// }
+
+// function repeat(num, func) {
+//   for (let i = 0; i < num; i++) {
+//     func();
+//   }
+// }
+
+// Storing functions in variables
 function greet() {
   console.log('Hi!, i Love You!!')
 }
@@ -6,14 +28,4 @@ function diss() {
   console.log('Your The waste!!');
 }
 
-function repeatThreeTimes(func) {
-  func();
-  func();
-  func();
-}
-
-function repeat(num, func) {
-  for (let i = 0; i < num; i++) {
-    func();
-  }
-}
+const funcs = [greet, diss];
